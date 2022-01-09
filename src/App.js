c95c3885a0d0d8b550c586f84957102e86160b7e
Erika Sandoval <erikasandoval-09@hotmail.com>
2022-01-09 14:13:18 -0300

    Avances

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
+// import 'bootstrap/dist/css/bootstrap.min.css'; 
 import {BrowserRouter, Route, Routes} from  'react-router-dom';
 import  Home  from "./components/Home";
 import './App.css';
 import Form from './components/Form';
 
+
 function App() {
   return (
     < >
-
-    
-      
         <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<Home/>} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
+// import { Container, Nav } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
     return (
         <>
+
             <h1>Navbar</h1>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <NavLink activeClassName="active" className="navbar-brand" exact to="/">Inicio</NavLink>
@@ -11,9 +13,25 @@ const Navbar = () => {
                 <NavLink activeClassName="active" className="navbar-brand" exact to="/">Recetas 2</NavLink>
                 <NavLink activeClassName="active" className="navbar-brand" exact to="/">Recetas 3</NavLink>
             </nav>
+
+            {/* <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">No country</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#">Recetas 1</Nav.Link>
+                    <Nav.Link href="#">Recetas 2</Nav.Link>
+                    <Nav.Link href="#">Recetas 3</Nav.Link>
+                   
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar> */}
         </>
     )
-
+            
 }
 
 export default Navbar;
